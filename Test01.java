9f6eef328afc157e040122ef80f0eaacff9a8e40
MenukaSankalpa <menuka.sankalpa@gmail.com>
2025-01-10 10:12:15 +0530

    create class for mathamatic operations user input

---
+++ Test01.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Test01 {
+    public static void main(String args[])
+    {
+        Scanner y = new Scanner(System.in);
+        Scanner x = new Scanner(System.in);
+
+
+
+
+    }
+}
