0e5abf53882e706cf87acee5b9876bd2f1b3f525
MenukaSankalpa <menuka.sankalpa@gmail.com>
2025-01-12 09:24:50 +0530

    create and(&&) operator updated

---
+++ logicaloperations.java
@@ -0,0 +1,17 @@
+public class logicaloperations {
+    public static void main (String args[])
+    {
+       int x = 20;
+       int y = -10;
+       if (x>0 && y>0)   //&& this and operator
+       {
+           System.out.println("Positive");
+       }
+       else {
+           System.out.println("Negative");
+
+       }
+
+
+    }
+}
