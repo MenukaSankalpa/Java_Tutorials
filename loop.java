3e2f86d943e220a0d7e9ce56fa7981e30833eae4
MenukaSankalpa <menuka.sankalpa@gmail.com>
2025-01-12 16:00:18 +0530

    make loop and test it

---
+++ loop.java
@@ -0,0 +1,14 @@
+public class loop {
+    public static void main(String args[])
+    {
+        int i = 1;
+
+        while(i<=10)
+        {
+            System.out.println("Hello");
+            i++; //i=i+1
+        }
+
+
+    }
+}
