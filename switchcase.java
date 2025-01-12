49d85c927f957cd2e8f942f2200cdaea65226b1e
MenukaSankalpa <menuka.sankalpa@gmail.com>
2025-01-12 09:46:38 +0530

    create switch query

---
+++ switchcase.java
@@ -0,0 +1,31 @@
+public class switchcase {
+    public static void main(String args[])
+    {
+        int weekday = 2;
+
+        switch(weekday)
+        {
+            case 1:
+                System.out.println("Today is Monday");
+                break;
+            case 2:
+                System.out.println("Today is Tuesday");
+                break;
+            case 3:
+                System.out.println("Today is Wednesday");
+                break;
+            case 4:
+                System.out.println("Today is Thursday");
+                break;
+            case 5:
+                System.out.println("Today is Friday");
+                break;
+            case 6:
+                System.out.println("Today is Satday");
+                break;
+            case 7:
+                System.out.println("Today is Sunday");
+                break;
+        }
+    }
+}
