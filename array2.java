cbe6662c061dda83db52ab0bde24d43276dd47dd
MenukaSankalpa <menuka.sankalpa@gmail.com>
2025-01-15 09:07:52 +0530

    dowhile array2 more   example create

---
+++ array2.java
@@ -0,0 +1,12 @@
+public class array2 {
+    public static void main(String args[])
+    {
+       int[][] x = {
+               {1, 2, 3},
+               {4, 5, 6},
+               {7, 8, 9}
+       };
+
+       System.out.println(x[2][1]);
+    }
+}
