571d7462297bad88761ce59b4b93256f60e3ee07
MenukaSankalpa <menuka.sankalpa@gmail.com>
2025-01-15 08:49:11 +0530

    dowhile lop example create

---
+++ dowhileloop.java
@@ -0,0 +1,12 @@
+public class dowhileloop {
+    public static void main(String args[])
+
+    {int i=17;
+        do{
+            System.out.println("hello");
+            i++;
+        }while(i<=10);
+
+    }
+
+}
