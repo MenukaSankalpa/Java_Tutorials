a3730f35a7afa15f19dc24a6bbaade88e6dae20c
MenukaSankalpa <menuka.sankalpa@gmail.com>
2025-01-15 08:57:24 +0530

    dowhile array  example create

---
+++ array.java
@@ -0,0 +1,9 @@
+public class array {
+    public static void main(String args[])
+    {
+        int[] numbers = {2, 4, 6, 8};
+        String[] names = {"Kamal", "Amal", "Nimal"};
+        names[0] = "pawan";
+        System.out.println(names[0]);
+    }
+}
