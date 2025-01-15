eda11f7069e1d812194bda82a8cc1d67a8eaadac
MenukaSankalpa <menuka.sankalpa@gmail.com>
2025-01-15 15:17:06 +0530

    dowhile forloop test  example create

---
+++ forloopwhile.java
@@ -0,0 +1,11 @@
+public class forloopwhile{
+    public static void main(String args[])
+    {
+        String[] names = {"amal", "kamal", "nimal"};
+        for(int i=0;i< names.length;i++)
+           // for(int i=0;i<3;i++)
+        {
+            System.out.println(names[i]);
+        }
+    }
+}
