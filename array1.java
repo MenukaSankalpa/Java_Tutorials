82b78837b75527eeabdd925f54d456c9638bcb6d
MenukaSankalpa <menuka.sankalpa@gmail.com>
2025-01-15 09:00:40 +0530

    dowhile array1  example create

---
+++ array1.java
@@ -0,0 +1,9 @@
+public class array1 {
+    public static void main(String args[])
+    {
+        String[] names = new String[5];
+        names[0] = "kamla";
+        names[1] = "amla";
+        System.out.println(names[1]);
+    }
+}
