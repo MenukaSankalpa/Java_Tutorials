4adfd4ad3c60e246dd31fcc69cd200cdb7daaa6d
MenukaSankalpa <menuka.sankalpa@gmail.com>
2025-01-17 12:18:40 +0530

    made data type and it

---
+++ datatype2.java
@@ -0,0 +1,18 @@
+public class datatype2 {
+    public static void main(String var[])
+    {
+        String str1 = "codewithmenuka";
+        String str2 = "gadaya dagaya";
+        System.out.println(str1.length());
+        System.out.println(str1.toUpperCase());
+        System.out.println(str1.toLowerCase());
+        System.out.println(str1+" "+str2);
+        System.out.println(str1.concat(" ").concat(str2));
+        System.out.println(str1.isEmpty());
+        System.out.println(str2.replace("gadaya","modaya"));
+        System.out.println(str2.contains("ga"));
+        //int x;
+        //String y;
+
+    }
+}
