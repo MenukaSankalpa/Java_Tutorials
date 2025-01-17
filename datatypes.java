e6f7302e5f0c66c81058f1e3c97bd895f3759540
MenukaSankalpa <menuka.sankalpa@gmail.com>
2025-01-17 09:32:10 +0530

    made data type and it

---
+++ datatypes.java
@@ -0,0 +1,16 @@
+public class datatypes {
+    public static void main(String args[])
+    {
+        //Primitive data types (int/boolean/double/float/float/char/short/long)
+        //Non-Primitive data types (String/Array/Class/interface)
+        String s1 = new String("Dog");
+        String s2 = new String("Frog");
+
+        System.out.println(s1);
+        System.out.println(s2);
+
+        char[] x = {'m', 'e', 'n', 'u', 'k', 'a'};
+        String str1 = new String(x);
+        System.out.println(str1);
+    }
+}
