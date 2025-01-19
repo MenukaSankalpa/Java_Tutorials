d0a6034fb27fcc9ed52d2871f4ed860a8aa4e72b
MenukaSankalpa <menuka.sankalpa@gmail.com>
2025-01-19 09:25:53 +0530

    java_constructors g=file create and coded

---
+++ java_constructors.java
@@ -0,0 +1,19 @@
+public class java_constructors {
+    java_constructors()
+    {
+        System.out.println("Welcome tom");
+    }
+    void meth()
+    {
+        System.out.println("Hello tom");
+
+    }
+
+    public static void main(String[] args)
+    {
+        java_constructors obj = new java_constructors();
+        obj.meth();
+
+    }
+
+}
