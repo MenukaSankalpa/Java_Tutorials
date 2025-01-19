ddb891f2ee8b13d022ef06b54ce60b99aa51eb21
MenukaSankalpa <menuka.sankalpa@gmail.com>
2025-01-19 08:50:42 +0530

    update static2

--- static2.java
+++ static2.java
@@ -1,13 +1,27 @@
 public class static2 {
     static String name;
+    static String name2;
+    static String name3;
+
 
     static void call(){
         System.out.println("take a call from "+name);
     }
+    static void call2(){
+        System.out.println("take a call from "+name2);
+    }
+    static void call3(){
+        System.out.println("take a call from "+name3);
+    }
     public static void main(String args[])
     {
         name = "nokiya";
+        name2 = "apple";
+        name3 = "ztc";
         call();
+        call2();
+        call3();
+
 
     }
 }
