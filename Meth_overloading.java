ef84c35f6fc40bb083774368b40270ccb8b0fb4d
MenukaSankalpa <menuka.sankalpa@gmail.com>
2025-01-23 15:54:23 +0530

    update method overloading polymorphism

---
+++ Meth_overloading.java
@@ -0,0 +1,10 @@
+public class Meth_overloading {
+    public void meth(){
+        System.out.println("method 1");
+    }
+
+    public static void main(String[] args) {
+        Meth_overloading obj = new Meth_overloading();
+        obj.meth();
+    }
+}
